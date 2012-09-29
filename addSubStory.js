ab922f2984524410ca40a78914a9cc0a40017287
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-09-29 17:14:42 +0200

    add dummy add button function with jquery

--- addSubStory.js
+++ addSubStory.js
@@ -4,4 +4,9 @@
 // @name           Add Sub-story
 // @include        https://agilezen.com/*
 // @description    Adds sub-story according to the requirements of Telenor MVNO
+// @require        http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
+
+(function () {
+    $('#top-panel').append('<button type="button" id="story-toolbar-delete" title="Permanently delete this story"><img src="/content/images/icons/delete.png">Delete</button>');
+}());
