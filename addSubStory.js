ac45686553a5f86524c7a9ad0464f3ecf85631bb
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-09-29 16:49:35 +0200

    add user script meta-data

--- addSubStory.js
+++ addSubStory.js
@@ -1,1 +1,7 @@
-﻿
+﻿// ==UserScript==
+// @author         Martin Rosén-Lidholm
+// @version        0.0.0
+// @name           Add Sub-story
+// @include        https://agilezen.com/*
+// @description    Adds sub-story according to the requirements of Telenor MVNO
+// ==/UserScript==
