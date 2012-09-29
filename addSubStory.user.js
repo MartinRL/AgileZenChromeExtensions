03d55147a0d921f96dfce972f1aef5a3df3f9c35
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-09-29 18:52:53 +0200

    add Sub button

---
+++ addSubStory.user.js
@@ -0,0 +1,29 @@
+﻿// ==UserScript==
+// @author         Martin Rosén-Lidholm
+// @version        0.0.0
+// @name           Add Sub-story
+// @include        https://agilezen.com/*
+// @include        http://agilezen.com/*
+// @description    Adds sub-story according to the requirements of Telenor MVNO
+// ==/UserScript==
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+    var script = document.createElement("script");
+    script.setAttribute("src", "//code.jquery.com/jquery-latest.min.js");
+    script.addEventListener('load', function () {
+        var script = document.createElement("script");
+        script.textContent = "(" + callback.toString() + ")();";
+        document.body.appendChild(script);
+    }, false);
+    document.body.appendChild(script);
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
+
+// the guts of this userscript
+function main() {
+//    alert($("#story-buttons").text());
+    $("#story-buttons").append("<button type='button' id='story-toolbar-sub' title='Danne en Small underopgave til denne story'><img src='/content/images/icons/add.png'>Sub</button>");
+}
