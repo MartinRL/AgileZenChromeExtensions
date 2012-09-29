16f0d8d38cb8ea6828796283f33c8436d4331788
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-09-29 17:22:35 +0200

    add include stmt

--- addSubStory.user.js
+++ addSubStory.user.js
@@ -3,10 +3,8 @@
 // @version        0.0.0
 // @name           Add Sub-story
 // @include        https://agilezen.com/*
+// @include        http://agilezen.com/*
 // @description    Adds sub-story according to the requirements of Telenor MVNO
 // @require        http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-(function () {
-    $('#top-panel').append('<button type="button" id="story-toolbar-delete" title="Permanently delete this story"><img src="/content/images/icons/delete.png">Delete</button>');
-}());
