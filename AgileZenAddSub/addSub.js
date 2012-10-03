0b06def553f918ea232c7857c75e3a66031c4330
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-10-03 19:59:43 +0200

    got xhr post to work for the first time - w00t

--- AgileZenAddSub/addSub.js
+++ AgileZenAddSub/addSub.js
@@ -1,1 +1,19 @@
-﻿alert("hello agile zen");
+﻿var addSubButtonHtml = "<button type='button' id='story-toolbar-sub' title='Danne en Small underopgave til denne story'><img src='/content/images/icons/add.png'>Sub</button>";
+
+$("#story-buttons").append(addSubButtonHtml);
+
+var createSubStory = function() {
+    alert("about to create sub!");
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        xhr.open('POST', 'https://agilezen.com/api/v1/projects/41632/stories', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader("X-Zen-ApiKey", "f4c92d749eb546c29fc964a7e84c1bfd");
+        xhr.onreadystatechange = function() {
+            alert(this.responseText);
+        };
+        xhr.send(JSON.stringify({ text: "test2" }));
+    }
+};
+
+$("#story-toolbar-sub").click(createSubStory);
