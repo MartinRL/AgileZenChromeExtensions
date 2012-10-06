d87e8a4d6feb7104d86f3de6aee1e47c8d5996a6
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-10-06 13:01:11 +0200

    change details according to spec

--- AddSub/addSub.js
+++ AddSub/addSub.js
@@ -1,15 +1,23 @@
 ﻿var addSubButtonHtml = "<button type='button' id='story-toolbar-sub' title='Danne en Small underopgave til denne story'><img src='/content/images/icons/add.png'>Sub</button>";
+var currentStoryDetails = "";
+var currentUrlSplitBySlash = window.location.pathname.split("/");
 
 $("#story-buttons").append(addSubButtonHtml);
 
+var setCurrentStoryDetails = function() {
+    var currentStory = getCurrentStory();
+    console.log("currentStory.details: " + currentStory.details);
+    currentStoryDetails = currentStory.details;
+};
+
 var getCurrentStoryNo = function() {
-    console.log("getCurrentStoryNo: " + window.location.pathname.split("/")[window.location.pathname.split("/").length - 1]);
-    return window.location.pathname.split("/")[window.location.pathname.split("/").length - 1];
+    console.log("getCurrentStoryNo: " + currentUrlSplitBySlash[currentUrlSplitBySlash.length - 1]);
+    return currentUrlSplitBySlash[currentUrlSplitBySlash.length - 1];
 };
 
 var getProjectNo = function() {
-    console.log("getProjectNo: " + window.location.pathname.split("/")[2]);
-    return window.location.pathname.split("/")[2];
+    console.log("getProjectNo: " + currentUrlSplitBySlash[2]);
+    return currentUrlSplitBySlash[2];
 };
 
 var getBaseApiUrl = function () {
@@ -38,29 +46,31 @@ var createSubStory = function() {
         xhrPost.open("POST", getBaseApiUrl(), true);
         xhrPost.setRequestHeader("Content-Type", "application/json");
         xhrPost.setRequestHeader("X-Zen-ApiKey", "f4c92d749eb546c29fc964a7e84c1bfd");
-        xhrPost.onreadystatechange = function() {
-            console.log("POST response: " + this.responseText);
-            var subStory = jQuery.parseJSON(this.responseText);
-            var currentStory = getCurrentStory();
-            var currentStoryDetails = "";
-            if (currentStory.details.indexOf("Implementeret i") == -1) {
-                currentStoryDetails += "Implementeret i <br />";
+        xhrPost.onreadystatechange = function () {
+            if (xhrPost.readyState === 4) {
+                console.log("POST response: " + this.responseText);
+                var subStory = jQuery.parseJSON(this.responseText);
+                if (currentStoryDetails.indexOf("Implementeret i") == -1) {
+                    currentStoryDetails = currentStoryDetails.concat("Implementeret i:");
+                }
+                currentStoryDetails = currentStoryDetails.concat("<br /> - #" + subStory.id);
+                var xhrUpdate = new XMLHttpRequest();
+                xhrUpdate.open("PUT", getBaseApiUrl() + "/" + getCurrentStoryNo(), true);
+                xhrUpdate.setRequestHeader("Content-Type", "application/json");
+                xhrUpdate.setRequestHeader("X-Zen-ApiKey", "f4c92d749eb546c29fc964a7e84c1bfd");
+                xhrUpdate.onreadystatechange = function () {
+                    console.log("PUT response: " + this.responseText);
+                };
+                console.log("currentStoryDetails to PUT: " + currentStoryDetails);
+                xhrUpdate.send(JSON.stringify({ details: currentStoryDetails }));
+//            location.reload(true);
             }
-            currentStoryDetails += " - #" + subStory.id;
-            var xhrUpdate = new XMLHttpRequest();
-            xhrUpdate.open("PUT", getBaseApiUrl() + "/" + getCurrentStoryNo(), true);
-            xhrUpdate.setRequestHeader("Content-Type", "application/json");
-            xhrUpdate.setRequestHeader("X-Zen-ApiKey", "f4c92d749eb546c29fc964a7e84c1bfd");
-            xhrUpdate.onreadystatechange = function() {
-                console.log("PUT response: " + this.responseText);
-            };
-            xhrUpdate.send(JSON.stringify({details: currentStoryDetails}));
         };
         var subStoryText = prompt("Venligst, angiv understorynavn.", "Lorem ipsum...");
         var tag = prompt("Venligst, angiv projekt-tag.", "Lorem ipsum...");
         var subStory = {
             text: subStoryText,
-            details: "Laves som en del af # ", /* aktuelt story# */
+            details: "Laves som en del af #" + getCurrentStoryNo(),
             size: "S",
             /*phase: pending / the step after backlog,*/
             /*owner: [username of current story],*/
@@ -70,4 +80,5 @@ var createSubStory = function() {
     }
 };
 
+setCurrentStoryDetails();
 $("#story-toolbar-sub").click(createSubStory);
