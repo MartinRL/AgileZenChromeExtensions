2f2d11d4f43995332fb58ecf018d21c96ecd4430
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-10-10 14:29:15 +0200

    delete console.log debug code

--- AddSub/options.js
+++ AddSub/options.js
@@ -1,5 +1,4 @@
 ﻿function saveOptions() {
-    console.log("saveOptions");
     var apiKey = document.getElementById("api_key").value;
     chrome.storage.sync.set({"api_key": apiKey});
 
@@ -8,18 +7,11 @@
     setTimeout(function () {
         status.innerHTML = "";
     }, 750);
-
-    chrome.storage.sync.get("api_key",
-        function(val) {
-            console.log("chrome.storage.sync.get['api_key']: " + JSON.stringify(val));
-        });
 }
 
 function restoreOptions() {
-    console.log("restoreOptions");
     chrome.storage.sync.get("api_key",
         function (apiKey) {
-            console.log("chrome.storage.sync.get['api_key']: " + JSON.stringify(apiKey));
             document.getElementById("api_key").value = apiKey.api_key;
         });
 }
