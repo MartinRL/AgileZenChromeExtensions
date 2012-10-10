63468ee0aae14df2ba7664c041b1b79f7b7dd06d
Martin Rosen-Lidholm <martin.rosenlidholm@gmail.com>
2012-10-10 20:03:56 +0200

    use jquery in options

--- AddSub/options.js
+++ AddSub/options.js
@@ -1,27 +1,27 @@
-﻿function saveOptions() {
-    var apiKey = document.getElementById("api_key").value;
-    var umbrellaDetailsLabel = document.getElementById("umbrella_details_label").value;
-    chrome.storage.sync.set({
-        "api_key": apiKey,
-        "umbrella_details_label": umbrellaDetailsLabel
-    });
-
-    var status = document.getElementById("status");
-    status.innerHTML = "options saved";
-    setTimeout(function () {
-        status.innerHTML = "";
-    }, 750);
-}
-
-function restoreOptions() {
-    chrome.storage.sync.get(null,
-        function (options) {
-            document.getElementById("api_key").value = options.api_key;
-            document.getElementById("umbrella_details_label").value = options.umbrella_details_label;
+﻿$(document).ready(function() {
+    var saveOptions = function() {
+        var apiKey = $("#api_key").val();
+        var umbrellaDetailsLabel = $("#umbrella_details_label").val();
+        chrome.storage.sync.set({
+            "api_key": apiKey,
+            "umbrella_details_label": umbrellaDetailsLabel
         });
-}
 
-window.onload = function() {
-    document.getElementById("save").addEventListener('click', saveOptions);
+        var status = $("#status");
+        status.html("options saved");
+        setTimeout(function() {
+            status.html("");
+        }, 750);
+    };
+
+    var restoreOptions = function() {
+        chrome.storage.sync.get(null,
+            function(options) {
+                $("#api_key").val(options.api_key);
+                $("#umbrella_details_label").val(options.umbrella_details_label);
+            });
+    };
+
+    $("#save").click(saveOptions);
     restoreOptions();
-};
+});
